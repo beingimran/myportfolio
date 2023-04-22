0e3f8a05821ae72c5acab7cbabe63317203882e5
mohammad imran <imranben35@gmail.com>
2023-04-22 14:28:08 +0530

    fixes

--- pages/crypto.jsx
+++ pages/crypto.jsx
@@ -37,13 +37,7 @@ const crypto = () => {
             application is dynamic routing through the &quot;React Router DOM&quot;
             package. This application was built using the Coin Gecko API.
           </p>
-          <a
-            href='https://github.com/fireclint/crypto-react-firebase'
-            target='_blank'
-            rel='noreferrer'
-          >
-            <button className='px-8 py-2 mt-4 mr-8'>Code</button>
-          </a>
+         
           <a
             href='https://cryptobase-yt.web.app/'
             target='_blank'
--- pages/twitch.jsx
+++ pages/twitch.jsx
@@ -42,14 +42,6 @@ const twitch = () => {
           >
             <button className='px-8 py-2 mt-4 mr-8'>Demo</button>
           </a>
-          {/* <a
-            href='https://github.com/fireclint/twitch-nextjs-tailwind'
-            target='_blank'
-            rel='noreferrer'
-          >
-            <button className='px-8 py-2 mt-4'>Code</button>
-          </a> */}
-
         </div>
         <div className='col-span-4 md:col-span-1 shadow-xl shadow-gray-400 rounded-xl py-4'>
           <div className='p-2'>
