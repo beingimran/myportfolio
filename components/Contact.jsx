7d8362ae90379b017ca5b5a084ac136dec3c56db
Dipti <diptisinghbhadouriya@gmail.com>
2025-01-29 23:05:45 +0530

    update images

--- components/Contact.jsx
+++ components/Contact.jsx
@@ -5,7 +5,6 @@ import { AiOutlineMail } from "react-icons/ai";
 import { BsFillPersonLinesFill } from "react-icons/bs";
 import { FaGithub, FaLinkedinIn } from "react-icons/fa";
 import { HiOutlineChevronDoubleUp } from "react-icons/hi";
-import ContactImg from "../public/assets/contact.jpg";
 
 const Contact = () => {
   return (
@@ -18,12 +17,13 @@ const Contact = () => {
         <div className="grid lg:grid-cols-5 gap-8">
           {/* left */}
           <div className="col-span-3 lg:col-span-2 w-full h-full shadow-xl shadow-gray-400 rounded-xl p-4">
-            <div className="lg:p-4 h-full ">
+            <div className="lg:p-1 h-full ">
               <div>
                 <Image
                   className="rounded-xl hover:scale-105 ease-in duration-300"
-                  src={ContactImg}
+                  src='/assets/contact.jpg'
                   alt="/"
+                  width={"400"} height={"400"}
                 />
               </div>
               <div>
